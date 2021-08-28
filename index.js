fa41eaa9740894124813879cebd7183c0069b867
Fabian <lindgrenfabian@gmail.com>
2021-08-28 17:23:40 +0200

    replace credentials file with environment variables

--- index.js
+++ index.js
@@ -1,67 +1,63 @@
 const fs = require('fs');
 const fetch = require('node-fetch');
+require('dotenv').config();
 
-fs.readFile("credentials.txt", "utf8", (err, data) => {
-    var data = data.replace("\r", "").split("\n");
+var authorization = "Bearer ";
+console.log(process.env)
+console.log(`Logging in as ${process.env.MSP_USERNAME}...\n`);
 
-    var username = data[0].trim();
-    var password = data[1].trim();
-    var userId = data[2].trim();
-    var authorization = "Bearer ";
+//get refresh token
+fetch("https://eu.mspapis.com/loginidentity/connect/token", {
+        "headers": {
+            "accept": "*/*",
+            "accept-language": "en-GB,en;q=0.9,en-US;q=0.8,sv;q=0.7",
+            "content-type": "application/x-www-form-urlencoded",
+            "sec-ch-ua": "\"Chromium\";v=\"92\", \" Not A;Brand\";v=\"99\", \"Microsoft Edge\";v=\"92\"",
+            "sec-ch-ua-mobile": "?0",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "cross-site"
+        },
+        "referrer": "https://moviestarplanet2.se/",
+        "referrerPolicy": "strict-origin-when-cross-origin",
+        "body": `client_id=unity.client&client_secret=secret&grant_type=password&scope=openid%20nebula%20offline_access&username=SE%7c${process.env.MSP_USERNAME}&password=${process.env.MSP_PASSWORD}&acr_values=gameId%3aj68d`,
+        "method": "POST",
+        "mode": "cors"
+    }).then(res => res.json())
+    .then(loginidentity => {
+        if (loginidentity.error) return console.error(`Error found`, loginidentity);
+        console.log(`Success! Logged in as ${process.env.MSP_USERNAME}.\n`)
 
-    console.log(`Logging in as ${username}...\n`)
-    //get refresh token
-    fetch("https://eu.mspapis.com/loginidentity/connect/token", {
-            "headers": {
-                "accept": "*/*",
-                "accept-language": "en-GB,en;q=0.9,en-US;q=0.8,sv;q=0.7",
-                "content-type": "application/x-www-form-urlencoded",
-                "sec-ch-ua": "\"Chromium\";v=\"92\", \" Not A;Brand\";v=\"99\", \"Microsoft Edge\";v=\"92\"",
-                "sec-ch-ua-mobile": "?0",
-                "sec-fetch-dest": "empty",
-                "sec-fetch-mode": "cors",
-                "sec-fetch-site": "cross-site"
-            },
-            "referrer": "https://moviestarplanet2.se/",
-            "referrerPolicy": "strict-origin-when-cross-origin",
-            "body": `client_id=unity.client&client_secret=secret&grant_type=password&scope=openid%20nebula%20offline_access&username=SE%7c${username}&password=${password}&acr_values=gameId%3aj68d`,
-            "method": "POST",
-            "mode": "cors"
-        }).then(res => res.json())
-        .then(loginidentity => {
-            if (loginidentity.error) return console.error(`Error found`, loginidentity);
-            console.log(`Success! Logged in as ${username}.\n`)
+        //get final bearer token
+        fetch("https://eu.mspapis.com/loginidentity/connect/token", {
+                "headers": {
+                    "accept": "*/*",
+                    "accept-language": "en-GB,en;q=0.9",
+                    "authorization": "Basic dW5pdHkuY2xpZW50OnNlY3JldA==",
+                    "content-type": "application/x-www-form-urlencoded",
+                    "sec-ch-ua": "\"Chromium\";v=\"92\", \" Not A;Brand\";v=\"99\", \"Microsoft Edge\";v=\"92\"",
+                    "sec-ch-ua-mobile": "?0",
+                    "sec-fetch-dest": "empty",
+                    "sec-fetch-mode": "cors",
+                    "sec-fetch-site": "cross-site"
+                },
+                "referrer": "https://moviestarplanet2.se/",
+                "referrerPolicy": "strict-origin-when-cross-origin",
+                "body": `grant_type=refresh_token&refresh_token=${loginidentity.refresh_token}&acr_values=gameId%3aj68d%20profileId%3a${process.env.MSP_ID}`,
+                "method": "POST",
+                "mode": "cors"
+            }).then(res => res.json())
+            .then(fullidentity => {
+                authorization += fullidentity.access_token;
 
-            //get final bearer token
-            fetch("https://eu.mspapis.com/loginidentity/connect/token", {
-                    "headers": {
-                        "accept": "*/*",
-                        "accept-language": "en-GB,en;q=0.9",
-                        "authorization": "Basic dW5pdHkuY2xpZW50OnNlY3JldA==",
-                        "content-type": "application/x-www-form-urlencoded",
-                        "sec-ch-ua": "\"Chromium\";v=\"92\", \" Not A;Brand\";v=\"99\", \"Microsoft Edge\";v=\"92\"",
-                        "sec-ch-ua-mobile": "?0",
-                        "sec-fetch-dest": "empty",
-                        "sec-fetch-mode": "cors",
-                        "sec-fetch-site": "cross-site"
-                    },
-                    "referrer": "https://moviestarplanet2.se/",
-                    "referrerPolicy": "strict-origin-when-cross-origin",
-                    "body": `grant_type=refresh_token&refresh_token=${loginidentity.refresh_token}&acr_values=gameId%3aj68d%20profileId%3a${userId}`,
-                    "method": "POST",
-                    "mode": "cors"
-                }).then(res => res.json())
-                .then(fullidentity => {
-                    authorization += fullidentity.access_token;
+                console.log(`Authorized as ${process.env.MSP_USERNAME}\n`);
 
-                    console.log(`Authorized as ${username}\n`);
+                pet(authorization);
+            });
+    });
 
-                    pet(userId, authorization);
-                });
-        });
-});
 
-function pet(userId, authorization) {
+function pet(authorization) {
     var pets = [
         /*"be3ec39825a8418ca67a9db50f187b5f",
          "bf445340671747558085b150a9f6520f",
@@ -104,7 +100,7 @@ function pet(userId, authorization) {
                 },
                 "referrer": "https://moviestarplanet2.se/",
                 "referrerPolicy": "strict-origin-when-cross-origin",
-                "body": `{"profileId":"${userId}","gameId":"j68d"}`,
+                "body": `{"profileId":"${process.env.MSP_ID}","gameId":"j68d"}`,
                 "method": "POST",
                 "mode": "cors"
             }).then(console.log(`Petted ${pets[i]}`));
